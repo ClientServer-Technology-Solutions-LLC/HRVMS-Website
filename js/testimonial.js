e24e08ced71a7163e558fa61191c634e4e32783b
Aravind-Maloth <aravind.maloth@clientservertech.com>
2025-07-04 17:39:17 +0530

    Image paths are changed

---
+++ js/testimonial.js
@@ -0,0 +1,46 @@
+// testimonials.js
+
+const testimonials = [
+  {
+    name: "Lance Jarvis",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt.",
+    image: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg",
+    bgClass: "green-bg",
+    social: true
+  },
+  {
+    name: "Ericka Lynda",
+    text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+    image: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg",
+    bgClass: "purple-bg",
+    social: false
+  },
+  {
+    name: "Neil Wilford",
+    text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.",
+    image: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg",
+    bgClass: "blue-bg",
+    social: true
+  },
+  {
+    name: "Sarah Thompson",
+    text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.",
+    image: "https://randomuser.me/api/portraits/women/44.jpg",
+    bgClass: "purple-bg",
+    social: true
+  },
+  {
+    name: "David Brown",
+    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.",
+    image: "https://randomuser.me/api/portraits/men/32.jpg",
+    bgClass: "green-bg",
+    social: false
+  },
+  {
+    name: "Linda White",
+    text: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+    image: "https://randomuser.me/api/portraits/women/55.jpg",
+    bgClass: "blue-bg",
+    social: true
+  }
+];
